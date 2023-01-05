e0e7cd4f24b6b4028957faf292076a5ac440e68d
nimgnas <sangminyn@gmail.com>
2023-01-06 00:29:12 +0900

    Auth 라우터, 폴더구조 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,20 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
-import Auth from "./Auth";
 import App from "./App";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Auth from "./pages/Auth";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "home",
-        element: <Home />,
-      },
+      { path: "home", element: <Home /> },
       {
         path: "auth",
         element: <Auth />,
+        children: [
+          { path: "login", element: <Login /> },
+          { path: "signup", element: <SignUp /> },
+        ],
       },
     ],
   },
