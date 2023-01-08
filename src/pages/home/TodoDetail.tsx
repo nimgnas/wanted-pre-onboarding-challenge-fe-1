8940577354c97b97133688d474507b288ae45312
nimgnas <sangminyn@gmail.com>
2023-01-08 23:32:44 +0900

    [STYLE] detail css

--- src/pages/home/TodoDetail.tsx
+++ src/pages/home/TodoDetail.tsx
@@ -3,7 +3,19 @@ import styled from "styled-components";
 function TodoDetail() {
   return (
     <Content>
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <Header>
+          <span className="arrow">﹤</span>
+          <span className="title">타이틀</span>
+          <span className="delete">X</span>
+        </Header>
+        <Body>
+          <p></p>
+        </Body>
+        <Footer>
+          <span>수정</span>
+        </Footer>
+      </Wrapper>
     </Content>
   );
 }
@@ -22,4 +34,51 @@ const Wrapper = styled.div`
   width: 100%;
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.black};
+`;
+
+const Header = styled.div`
+  height: 50px;
+  padding: 10px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  span {
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  .arrow,
+  .delete {
+    &:hover {
+      color: ${({ theme }) => theme.colors.blue};
+      cursor: pointer;
+    }
+  }
+`;
+
+const Body = styled.div`
+  height: 210px;
+  padding: 10px 20px;
+
+  p {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+const Footer = styled.div`
+  height: 40px;
+  padding: 10px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  span {
+    color: ${({ theme }) => theme.colors.white};
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.blue};
+      cursor: pointer;
+    }
+  }
 `;
